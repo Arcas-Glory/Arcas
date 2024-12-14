caa90742ced5de2faafe3f50c82ddbf930ccba8a
Shi-cai <591616292@qq.com>
2024-12-14 18:09:09 +0800

    v0.4

---
+++ Arcas.Server/Models/Artical.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace sqlTest.Server.Models
+{
+    public class Artical
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int id { get; set; }
+        public int userId { get; set; }
+        public int bookId { get; set; }
+        public string title { get; set; }
+        public string context { get; set; }
+    }
+}
